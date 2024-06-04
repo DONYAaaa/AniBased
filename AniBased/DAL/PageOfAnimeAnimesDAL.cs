badeaa0717124615644b09abef99e7dd499366b2
Данил Суров <danil.surov.03@mail.ru>
2024-06-05 04:32:23 +0700

    yes

--- AniBased/DAL/PageOfAnimeAnimesDAL.cs
+++ AniBased/DAL/PageOfAnimeAnimesDAL.cs
@@ -8,5 +8,8 @@ namespace AniBased.DAL
 {
     internal class PageOfAnimeAnimesDAL
     {
+        public int Id { get; set; }
+        public int AnimesId { get; set; }
+        public int PageOfAnimeId { get; set; }
     }
 }
